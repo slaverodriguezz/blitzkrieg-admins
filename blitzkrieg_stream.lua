9a93990525080176c8ddab2e13bb588734a5b334
slaverodriguezz <lukagogishvili505@gmail.com>
2026-01-21 20:48:51 +0400

    Update blitzkrieg_stream.lua

--- blitzkrieg_stream.lua
+++ blitzkrieg_stream.lua
@@ -3,7 +3,7 @@ local encoding = require 'encoding'
 encoding.default = 'CP1251'
 local u8 = encoding.UTF8
 
-local script_version = 1
+local script_version = 2
 local script_url = "https://raw.githubusercontent.com/slaverodriguezz/blitzkrieg-admins/main/blitzkrieg_stream.lua"
 local script_path = thisScript().path
 
@@ -18,7 +18,7 @@ function main()
     
     checkUpdate()
 
-    sampAddChatMessage(string.format("{%s}[blitzkrieg] {FFFFFF}Чеккер загружен. v%d | Автор: {7B70FA}slave_rodriguez", mainColor, script_version), -1)
+    sampAddChatMessage(string.format("{%s}[blitzkrieg] {FFFFFF}Чеккер читеров загружен. v%d | Команды: /stream, /maxlvl, /sfc | Автор: {7B70FA}slave_rodriguez", mainColor, script_version), -1)
 
     sampRegisterChatCommand("maxlvl", function(arg)
         if #arg > 0 and tonumber(arg) then
@@ -31,36 +31,52 @@ function main()
 
     sampRegisterChatCommand("sfc", function()
         local _, myId = sampGetPlayerIdByCharHandle(PLAYER_PED)
+        local myColor = sampGetPlayerColor(myId)
         local ids = {}
+        
         for i = 0, 999 do
             local result, ped = sampGetCharHandleBySampPlayerId(i)
             if result and doesCharExist(ped) and i ~= myId then
-                if sampGetPlayerScore(i) <= maxLevelLimit then table.insert(ids, tostring(i)) end
+                local playerColor = sampGetPlayerColor(i)
+                local level = sampGetPlayerScore(i)
+                
+                if level <= maxLevelLimit and playerColor ~= myColor then 
+                    table.insert(ids, tostring(i)) 
+                end
             end
         end
+        
         if #ids > 0 then
             sampSendChat(string.format("/fc Читеры: %s", table.concat(ids, " ")))
         else
-            sampAddChatMessage(string.format("{%s}[blitzkrieg] {FFFFFF}Читеров нет.", mainColor), -1)
+            sampAddChatMessage(string.format("{%s}[blitzkrieg] {FFFFFF}Читеров нет (или у всех твой клист).", mainColor), -1)
         end
     end)
 
     sampRegisterChatCommand("stream", function()
         local _, myId = sampGetPlayerIdByCharHandle(PLAYER_PED)
+        local myColor = sampGetPlayerColor(myId)
+        
         sampAddChatMessage(string.format("{%s}[blitzkrieg] Список читеров в зоне стрима [до %d уровня]", mainColor, maxLevelLimit), -1)
         local count = 0
+        
         for i = 0, 999 do
             local result, ped = sampGetCharHandleBySampPlayerId(i)
             if result and doesCharExist(ped) and i ~= myId then
                 local level = sampGetPlayerScore(i)
-                if level <= maxLevelLimit then
-                    local hexColor = string.format("%06X", bit.band(sampGetPlayerColor(i), 0xFFFFFF))
+                local playerColor = sampGetPlayerColor(i)
+                
+                if level <= maxLevelLimit and playerColor ~= myColor then
+                    local hexColor = string.format("%06X", bit.band(playerColor, 0xFFFFFF))
                     sampAddChatMessage(string.format("{%s}%s{%s}[%d] | Level: %d", hexColor, sampGetPlayerNickname(i), notmainColor, i, level), -1)
                     count = count + 1
                 end
             end
         end
-        if count == 0 then sampAddChatMessage(string.format("{%s}В зоне стрима нет подходящих игроков.", mainColor), -1) end
+        
+        if count == 0 then 
+            sampAddChatMessage(string.format("{%s}В зоне стрима нет подходящих игроков (кроме своих).", mainColor), -1) 
+        end
     end)
 
     wait(-1)
@@ -68,20 +84,16 @@ end
 
 function checkUpdate()
     local temp_path = getWorkingDirectory() .. "\\temp_stream.lua"
-    
     downloadUrlToFile(script_url, temp_path, function(id, status, p1, p2)
         if status == 6 then
             local f = io.open(temp_path, "r")
             if f then
                 local content = f:read("*a")
                 f:close()
-                
                 local new_version = content:match("local script_version = (%d+)")
                 new_version = tonumber(new_version)
-
                 if new_version and new_version > script_version then
                     sampAddChatMessage(string.format("{%s}[blitzkrieg] {FFFFFF}Найдено обновление v%d! Устанавливаю...", mainColor, new_version), -1)
-                    
                     local new_file = io.open(script_path, "w")
                     if new_file then
                         new_file:write(content)
