3a9810a95ea3df703b42e8f8ac569d097da58056
slaverodriguezz <lukagogishvili505@gmail.com>
2026-01-21 20:42:19 +0400

    Create blitzkrieg_stream.lua

---
+++ blitzkrieg_stream.lua
@@ -0,0 +1,96 @@
+local samp = require 'lib.samp.events'
+local encoding = require 'encoding'
+encoding.default = 'CP1251'
+local u8 = encoding.UTF8
+
+local script_version = 1
+local script_url = "https://raw.githubusercontent.com/ТВОЙ_НИК/РЕПОЗИТОРИЙ/main/stream.lua"
+local script_path = thisScript().path
+
+local maxLevelLimit = 999
+local mainColor = "7B70FA"
+local notmainColor = "FFFFFF"
+local errorColor = "FF0000"
+
+function main()
+    if not isSampLoaded() or not isSampfuncsLoaded() then return end
+    while not isSampAvailable() do wait(100) end
+    
+    checkUpdate()
+
+    sampAddChatMessage(string.format("{%s}[blitzkrieg] {FFFFFF}Чеккер загружен. v%d | Автор: {7B70FA}slave_rodriguez", mainColor, script_version), -1)
+
+    sampRegisterChatCommand("maxlvl", function(arg)
+        if #arg > 0 and tonumber(arg) then
+            maxLevelLimit = tonumber(arg)
+            sampAddChatMessage(string.format("{%s}[blitzkrieg] Максимальный уровень: {FFFFFF}%d", mainColor, maxLevelLimit), -1)
+        else
+            sampAddChatMessage(string.format("{%s}[Ошибка] Используйте: /maxlvl [число]", errorColor), -1)
+        end
+    end)
+
+    sampRegisterChatCommand("sfc", function()
+        local _, myId = sampGetPlayerIdByCharHandle(PLAYER_PED)
+        local ids = {}
+        for i = 0, 999 do
+            local result, ped = sampGetCharHandleBySampPlayerId(i)
+            if result and doesCharExist(ped) and i ~= myId then
+                if sampGetPlayerScore(i) <= maxLevelLimit then table.insert(ids, tostring(i)) end
+            end
+        end
+        if #ids > 0 then
+            sampSendChat(string.format("/fc Читеры: %s", table.concat(ids, " ")))
+        else
+            sampAddChatMessage(string.format("{%s}[blitzkrieg] {FFFFFF}Читеров нет.", mainColor), -1)
+        end
+    end)
+
+    sampRegisterChatCommand("stream", function()
+        local _, myId = sampGetPlayerIdByCharHandle(PLAYER_PED)
+        sampAddChatMessage(string.format("{%s}[blitzkrieg] Список читеров в зоне стрима [до %d уровня]", mainColor, maxLevelLimit), -1)
+        local count = 0
+        for i = 0, 999 do
+            local result, ped = sampGetCharHandleBySampPlayerId(i)
+            if result and doesCharExist(ped) and i ~= myId then
+                local level = sampGetPlayerScore(i)
+                if level <= maxLevelLimit then
+                    local hexColor = string.format("%06X", bit.band(sampGetPlayerColor(i), 0xFFFFFF))
+                    sampAddChatMessage(string.format("{%s}%s{%s}[%d] | Level: %d", hexColor, sampGetPlayerNickname(i), notmainColor, i, level), -1)
+                    count = count + 1
+                end
+            end
+        end
+        if count == 0 then sampAddChatMessage(string.format("{%s}В зоне стрима нет подходящих игроков.", mainColor), -1) end
+    end)
+
+    wait(-1)
+end
+
+function checkUpdate()
+    local temp_path = getWorkingDirectory() .. "\\temp_stream.lua"
+    
+    downloadUrlToFile(script_url, temp_path, function(id, status, p1, p2)
+        if status == 6 then
+            local f = io.open(temp_path, "r")
+            if f then
+                local content = f:read("*a")
+                f:close()
+                
+                local new_version = content:match("local script_version = (%d+)")
+                new_version = tonumber(new_version)
+
+                if new_version and new_version > script_version then
+                    sampAddChatMessage(string.format("{%s}[blitzkrieg] {FFFFFF}Найдено обновление v%d! Устанавливаю...", mainColor, new_version), -1)
+                    
+                    local new_file = io.open(script_path, "w")
+                    if new_file then
+                        new_file:write(content)
+                        new_file:close()
+                        sampAddChatMessage(string.format("{%s}[blitzkrieg] {FFFFFF}Обновлено успешно! Нажми {7B70FA}Ctrl + R", mainColor), -1)
+                    end
+                end
+                os.remove(temp_path)
+            end
+        end
+    end)
+end
