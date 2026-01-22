bf9888acbcb9440e45f9d0001dcca2196cebc2aa
slaverodriguezz <lukagogishvili505@gmail.com>
2026-01-22 11:47:15 +0400

    Update blitzkrieg_stream.lua

--- blitzkrieg_stream.lua
+++ blitzkrieg_stream.lua
@@ -3,7 +3,7 @@ local encoding = require 'encoding'
 encoding.default = 'CP1251'
 local u8 = encoding.UTF8
 
-local script_version = 7
+local script_version = 8
 local script_url = "https://raw.githubusercontent.com/slaverodriguezz/blitzkrieg-admins/main/blitzkrieg_stream.lua"
 local script_path = thisScript().path
 
@@ -12,18 +12,13 @@ local mainColor = "7B70FA"
 local notmainColor = "FFFFFF"
 local errorColor = "FF0000"
 
-local showScreenList = false
-local screenPos = {x = 500, y = 500}
-local isDragging = false
-local font = renderCreateFont("Arial", 10, 5) 
-
 function main()
     if not isSampLoaded() or not isSampfuncsLoaded() then return end
     while not isSampAvailable() do wait(100) end
     
     checkUpdate()
 
-    sampAddChatMessage(string.format(u8"{%s}[blitzkrieg] {FFFFFF}Cheaters checker v%d loaded. | /stream, /sfc, /sstream", mainColor, script_version), -1)
+    sampAddChatMessage(string.format(u8"{%s}[blitzkrieg] {FFFFFF}Cheaters checker v%d loaded. | commands: /stream, /maxlvl, /sfc | author: {7B70FA}slave_rodriguez", mainColor, script_version), -1)
 
     sampRegisterChatCommand("maxlvl", function(arg)
         if #arg > 0 and tonumber(arg) then
@@ -38,87 +33,52 @@ function main()
         local _, myId = sampGetPlayerIdByCharHandle(PLAYER_PED)
         local myColor = sampGetPlayerColor(myId)
         local ids = {}
+        
         for i = 0, 999 do
             local result, ped = sampGetCharHandleBySampPlayerId(i)
             if result and doesCharExist(ped) and i ~= myId then
-                if sampGetPlayerScore(i) <= maxLevelLimit and sampGetPlayerColor(i) ~= myColor then 
+                local playerColor = sampGetPlayerColor(i)
+                local level = sampGetPlayerScore(i)
+                if level <= maxLevelLimit and playerColor ~= myColor then 
                     table.insert(ids, tostring(i)) 
                 end
             end
         end
-        if #ids > 0 then sampSendChat(string.format("/fc Cheaters: %s", table.concat(ids, " ")))
-        else sampAddChatMessage(u8"{7B70FA}[blitzkrieg] {FFFFFF}Никого не найдено", -1) end
+        
+        if #ids > 0 then
+            sampSendChat(string.format(u8"/fc Cheaters: %s", table.concat(ids, " ")))
+        else
+            sampAddChatMessage(string.format(u8"{%s}[blitzkrieg] {FFFFFF}No cheaters found.", mainColor), -1)
+        end
     end)
 
     sampRegisterChatCommand("stream", function()
         local _, myId = sampGetPlayerIdByCharHandle(PLAYER_PED)
         local myColor = sampGetPlayerColor(myId)
-        sampAddChatMessage(string.format(u8"{%s}[blitzkrieg] Stream zone [up to %d lvl]", mainColor, maxLevelLimit), -1)
+        
+        sampAddChatMessage(string.format(u8"{%s}[blitzkrieg] List of cheater in stream zone [up to %d lvl]", mainColor, maxLevelLimit), -1)
+        local count = 0
+        
         for i = 0, 999 do
             local result, ped = sampGetCharHandleBySampPlayerId(i)
             if result and doesCharExist(ped) and i ~= myId then
                 local level = sampGetPlayerScore(i)
                 local playerColor = sampGetPlayerColor(i)
+                
                 if level <= maxLevelLimit and playerColor ~= myColor then
                     local hexColor = string.format("%06X", bit.band(playerColor, 0xFFFFFF))
                     sampAddChatMessage(string.format("{%s}%s{%s}[%d] | Level: %d", hexColor, sampGetPlayerNickname(i), notmainColor, i, level), -1)
+                    count = count + 1
                 end
             end
         end
+        
+        if count == 0 then 
+            sampAddChatMessage(string.format(u8"{%s}No cheaters found.", mainColor), -1) 
+        end
     end)
 
-    sampRegisterChatCommand("sstream", function()
-        showScreenList = not showScreenList
-        sampAddChatMessage(u8"{7B70FA}[blitzkrieg] {FFFFFF}Экранный список: " .. (showScreenList and "{00FF00}ВКЛ" or "{FF0000}ВЫКЛ"), -1)
-    end)
-
-    while true do
-        wait(0)
-        if showScreenList then
-            drawList()
-            handleDragging()
-        end
-    end
-end
-
-function drawList()
-    local _, myId = sampGetPlayerIdByCharHandle(PLAYER_PED)
-    local myColor = sampGetPlayerColor(myId)
-    local y = screenPos.y
-    
-    renderFontDrawText(font, "Stream List (Drag me)", screenPos.x, y, 0xFF7B70FA)
-    y = y + 15
-
-    for i = 0, 999 do
-        local result, ped = sampGetCharHandleBySampPlayerId(i)
-        if result and doesCharExist(ped) and i ~= myId then
-            local level = sampGetPlayerScore(i)
-            local pCol = sampGetPlayerColor(i)
-            if level <= maxLevelLimit and pCol ~= myColor then
-                local nick = sampGetPlayerNickname(i)
-                local renderColor = bit.or(bit.band(pCol, 0xFFFFFF), 0xFF000000) 
-                renderFontDrawText(font, string.format("%s[%d] | Lvl: %d", nick, i, level), screenPos.x, y, renderColor)
-                y = y + 15
-            end
-        end
-    end
-end
-
-function handleDragging()
-    if isSampAvailable() and sampIsCursorActive() then
-        local cX, cY = getCursorPos()
-        if isKeyDown(0x01) then 
-            if not isDragging and cX >= screenPos.x and cX <= screenPos.x + 150 and cY >= screenPos.y and cY <= screenPos.y + 20 then
-                isDragging = true
-            end
-            if isDragging then
-                screenPos.x = cX - 75
-                screenPos.y = cY - 10
-            end
-        else
-            isDragging = false
-        end
-    end
+    wait(-1)
 end
 
 function checkUpdate()
@@ -130,12 +90,14 @@ function checkUpdate()
                 local content = f:read("*a")
                 f:close()
                 local new_version = content:match("local script_version = (%d+)")
-                if new_version and tonumber(new_version) > script_version then
+                new_version = tonumber(new_version)
+                if new_version and new_version > script_version then
+                    sampAddChatMessage(string.format(u8"{%s}[blitzkrieg] {FFFFFF}Update is found v%d! Downloading...", mainColor, new_version), -1)
                     local new_file = io.open(script_path, "w")
                     if new_file then
                         new_file:write(content)
                         new_file:close()
-                        sampAddChatMessage(u8"{7B70FA}[blitzkrieg] {FFFFFF}Обновлено! Перезагрузите скрипты (Ctrl+R)", -1)
+                        sampAddChatMessage(string.format(u8"{%s}[blitzkrieg] {FFFFFF}Updated! Press {7B70FA}Ctrl + R", mainColor), -1)
                     end
                 end
                 os.remove(temp_path)
