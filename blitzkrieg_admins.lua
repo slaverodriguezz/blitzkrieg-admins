dfc0b31af68b016114bd4b9d74449348bf551f98
slaverodriguezz <lukagogishvili505@gmail.com>
2026-02-06 18:30:00 +0400

    Update blitzkrieg_admins.lua

--- blitzkrieg_admins.lua
+++ blitzkrieg_admins.lua
@@ -1,12 +1,12 @@
 script_name("blitzkrieg admins")
 script_author("slave_rodriguez")
-script_version("3.1")
+script_version("3.2")
 
 require "lib.moonloader"
 local sampev = require "lib.samp.events"
 local requests = require("requests")
 
-local SCRIPT_VERSION = "3.1" 
+local SCRIPT_VERSION = "3.2" 
 local SCRIPT_URL = "https://raw.githubusercontent.com/slaverodriguezz/blitzkrieg-admins/main/blitzkrieg_admins.lua"
 local SCRIPT_PATH = getWorkingDirectory() .. "\\blitzkrieg_admins.lua"
 local textColor = "{F5DEB3}"
@@ -44,7 +44,7 @@ local admins = {
     ["Test_Evlv"] = 8, ["Domenick_Jackson"] = 8, ["Fernando_Bennet"] = 6,
     ["Egor_Ufimtsev"] = 6, ["Daniel_Salaru"] = 6, ["Wilion_Walker"] = 5, ["Rikuto_Yashida"] = 5,
     ["Aleksei_Kuznetcov"] = 5, ["Anthony_Cerezo"] = 5, ["Niko_Filliams"] = 5,
-    ["Ramon_Morettie"] = 5, ["Alessandro_Carrasco"] = 5, ["Midzuki_Cerezo"] = 3,
+    ["Ramon_Morettie"] = 5, ["Alessandro_Carrasco"] = 5, ["Midzuki_Cerezo"] = 4,
     ["Kwenyt_Joestar"] = 3, ["Absolutely_Sawide"] = 5, ["Oruto_Matsushima"] = 5,
     ["Michael_Rojas"] = 6, ["Marco_Mazzini"] = 5, ["Edward_Thawne"] = 5, ["Mayu_Sakura"] = 5,
     ["Donatello_Ross"] = 5, ["Cody_Flatcher"] = 5, ["Carlo_Barbero"] = 5, ["Ruslan_Satriano"] = 5,
@@ -52,9 +52,9 @@ local admins = {
     ["Faust_Casso"] = 3, ["Yuliya_Ermak"] = 5, ["Mickey_Marryman"] = 5,
     ["Jayden_Henderson"] = 5, ["Arteezy_Adalwolff"] = 5, ["Mayson_Wilson"] = 5, ["Denis_MacTavish"] = 5,
     ["Laurent_Lemieux"] = 5, ["Simon_Frolov"] = 5, ["Dimentii_Lazarev"] = 5, ["Sandy_Blum"] = 5, 
-    ["Yaroslav_Yarkin"] = 5, ["Kira_Yukimura"] = 5, ["Gracie_Ludvig"] = 5, ["Artem_Rosenberg"] = 5, 
-    ["Emmett_Hoggarth"] = 5, ["Temik_Attano"] = 1, ["Chapa_Winx"] = 1, ["Calvin_Broadus"] = 1, 
-    ["Rabbit_Tomioka"] = 1, ["Shiro_Mercedez"] = 1, ["Christian_Moon"] = 1, ["Aitesu_Matsumoto"] = 1, 
+    ["Kira_Yukimura"] = 5, ["Gracie_Ludvig"] = 5, ["Artem_Rosenberg"] = 5, 
+    ["Emmett_Hoggarth"] = 5, ["Temik_Attano"] = 2, ["Chapa_Winx"] = 2, ["Calvin_Broadus"] = 2, 
+    ["Rabbit_Tomioka"] = 2, ["Shiro_Mercedez"] = 2, ["Christian_Moon"] = 2, ["Aitesu_Matsumoto"] = 2, 
     ["William_Bueno"] = 4, ["Nate_River"] = 4, ["Kasper_Whiter"] = 3
 }
 
@@ -139,6 +139,7 @@ function cmd_fcadmins()
     local allIds = table.concat(adminIds, ", ")
     sampSendChat("/fc Admins online: " .. allIds)
 end
+
 
 
 
